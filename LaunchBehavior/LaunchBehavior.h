f6b59d027fbb1a09d13d47345c7e6c530f18b612
yutmr <yu.ampm@gmail.com>
2018-05-13 11:48:25 +0900

    Initial Commit

---
+++ LaunchBehavior/LaunchBehavior.h
@@ -0,0 +1,19 @@
+//
+//  LaunchBehavior.h
+//  LaunchBehavior
+//
+//  Created by Yu Tamura on 2018/05/13.
+//  Copyright © 2018年 Yu Tamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LaunchBehavior.
+FOUNDATION_EXPORT double LaunchBehaviorVersionNumber;
+
+//! Project version string for LaunchBehavior.
+FOUNDATION_EXPORT const unsigned char LaunchBehaviorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaunchBehavior/PublicHeader.h>
+
+
